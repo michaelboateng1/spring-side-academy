fe3527bb0364bc6b4409c532377893b080a09fbd
Michael Boateng <michaelboateng064@gmail.com>
2026-01-22 15:07:04 +0000

    Added supabase client and a middleware to handle routing

---
+++ src/hooks.server.js
@@ -0,0 +1,20 @@
+import { supabase } from '$lib/supabaseClient';
+import { redirect } from '@sveltejs/kit';
+
+function getUserSession(){
+    supabase.auth.onAuthStateChange((_event, session) => {
+        console.log(session?.user);
+    });
+}
+
+export async function handle({ event, resolve }) {
+
+	if (event.url.pathname === '/dashboard') {
+        if(getUserSession()) return redirect(302, "/dashboard");
+        
+        return redirect(302, "/dashboard-login");
+	}
+
+	const response = await resolve(event);
+	return response;
+}
