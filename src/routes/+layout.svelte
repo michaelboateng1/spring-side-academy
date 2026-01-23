fe400cdab6aa4e275ab21f37d37dd523a6425d38
Michael <michaelboateng064@gmail.com>
2026-01-23 13:06:58 +0000

    implemented the loader component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,26 @@
 <script>
+	import {onMount} from "svelte";
+	import Loader from './components/Loader.svelte';
+
+	let loading = $state(true);
+
+	onMount(() => {
+		const removeLoader = setTimeout(() => {
+			loading = false;
+		}, 2000);
+
+		return () => clearTimeout(removeLoader);
+	})
+
 	let { children } = $props();
 </script>
 
-<main>
-	{@render children?.()}
-</main>
+{#if loading}
+	<Loader />
+{:else}
+	<main>
+		{@render children?.()}
+	</main>
+{/if}
+
 
