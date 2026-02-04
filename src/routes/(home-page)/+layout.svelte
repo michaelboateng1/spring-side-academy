4a17a7fcc754dcf054c9bb60c17f5adac19a8909
Michael <michaelboateng064@gmail.com>
2026-02-04 09:53:50 +0000

    Updated the metadata

--- src/routes/(home-page)/+layout.svelte
+++ src/routes/(home-page)/+layout.svelte
@@ -28,6 +28,17 @@
 		name="description"
 		content="A modern, nurturing K-12 school focused on excellence, creativity, and character."
 	/>
+	<meta property="og:title" content="Spring Side Academy" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://spring-side-academy.netlify.app/" />
+	<meta property="og:image" content="https://scontent.facc5-2.fna.fbcdn.net/v/t39.30808-6/475119007_644681824739817_3993957919059475782_n.jpg?stp=dst-jpg_p526x296_tt6&_nc_cat=101&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeGsNg7g9Lnkf8ko2lnUxrcANNv7yiwLpn402_vKLAumfsGFGYuUpTykdEt2F4AwKwEcjqfq8fbC1gu-kmEbnD-K&_nc_ohc=gxkxBOy1oCsQ7kNvwEk3GFo&_nc_oc=AdlDyi_Lv3-XNP2KRYqWBd1uA2fScp_cHGyPIEBCVkD3_9IsywiOeAwvJJnLc7LfyxQ&_nc_zt=23&_nc_ht=scontent.facc5-2.fna&_nc_gid=ggzcGzj1HB4vLoz_hGUYGw&oh=00_Afvi3pHkGgcBVI2pAWeCnYF_yLxjqDO5Mbo9Xe2hUa3sww&oe=6988D8EB" />
+	<meta property="og:image:secure_url" content="https://scontent.facc5-2.fna.fbcdn.net/v/t39.30808-6/475119007_644681824739817_3993957919059475782_n.jpg?stp=dst-jpg_p526x296_tt6&_nc_cat=101&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeGsNg7g9Lnkf8ko2lnUxrcANNv7yiwLpn402_vKLAumfsGFGYuUpTykdEt2F4AwKwEcjqfq8fbC1gu-kmEbnD-K&_nc_ohc=gxkxBOy1oCsQ7kNvwEk3GFo&_nc_oc=AdlDyi_Lv3-XNP2KRYqWBd1uA2fScp_cHGyPIEBCVkD3_9IsywiOeAwvJJnLc7LfyxQ&_nc_zt=23&_nc_ht=scontent.facc5-2.fna&_nc_gid=ggzcGzj1HB4vLoz_hGUYGw&oh=00_Afvi3pHkGgcBVI2pAWeCnYF_yLxjqDO5Mbo9Xe2hUa3sww&oe=6988D8EB" />
+	<meta property="og:image:width" content="400" />
+<meta property="og:image:height" content="300" />
+<meta property="og:image:alt" content="Spring Side Academy" />
+	<meta property="og:description" 
+  content="Spring Side Academy is a school within the Kumasi catchment area, specifically at Atasomanso. Our thriving school community includes Nursery and Primary classes." />
+  <meta property="og:locale" content="en_US" />
 </svelte:head>
 
 
